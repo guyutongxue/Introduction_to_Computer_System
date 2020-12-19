ba5a3b7975ef25b1b4a6faf7470923ee3a18353c
Guyutongxue <guyutongxue@163.com>
2020-12-19 17:16:27 +0800

    bug bug bug

--- malloclab-handout/mm.c
+++ malloclab-handout/mm.c
@@ -14,6 +14,7 @@
  */
 
 #include <assert.h>
+#include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -54,7 +55,7 @@
 // Alignment set to double word.
 #define ALIGNMENT 8
 
-// rounds up to the nearest multiple of ALIGNMENT
+/// Rounds up to the nearest multiple of @c ALIGNMENT
 #define ALIGN(p) (((size_t)(p) + ((ALIGNMENT)-1)) & ~0x7)
 
 // Basic types and sizes
@@ -66,10 +67,10 @@ typedef uint64_t DWORD;  ///< DWORD (double WORD) is 64-bit
 /* Explanation of a block:
  *
  * Allocated:
- * +--------+-------------------------------+--------+
- * | HEADER |     PAYLOAD (incl. align)     | FOOTER |
- * +--------+-------------------------------+--------+
- *     4B   ^       GET_SIZE(bp)          4B
+ * +--------+----------------------------------------+
+ * | HEADER |         PAYLOAD (incl. align)          |
+ * +--------+----------------------------------------+
+ *     4B   ^
  *          |
  *          bp (base pointer)
  *
@@ -81,6 +82,9 @@ typedef uint64_t DWORD;  ///< DWORD (double WORD) is 64-bit
  *          |
  *          bp (base pointer)
  *
+ * MIN_PAYLOAD_SIZE == 20B
+ * MIN_BLOCK_SIZE   == 24B
+ *
  * HEADER & FOOTER:
  * +-------------------------------------+---+---+---+
  * |        SIZE (first 29 bits)         |   | B | A |
@@ -90,12 +94,17 @@ typedef uint64_t DWORD;  ///< DWORD (double WORD) is 64-bit
  * B - Boundary tag
  *
  */
+#define MIN_PAYLOAD_SIZE 20   ///< Minimal payload alignment
 #define MIN_BLOCK_SIZE 24     ///< Minimal block size
 #define INIT_SIZE (1 << 6)    ///< Initial heap size
 #define CHUNK_SIZE (1 << 12)  ///< Extend heap by this amount
 
-/// Get the larger value of @c x and @c y
+#define CHECK_HEAP() mm_checkheap_alter(__func__, __LINE__)
+
+/// Get the greater value of @c x and @c y
 #define MAX(x, y) ((x) > (y) ? (x) : (y))
+/// Get the less value of @c x and @c y
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
 
 /// Error return value of functions return pointer
 #define ERRPTR ((void*)-1)
@@ -135,6 +144,7 @@ typedef uint64_t DWORD;  ///< DWORD (double WORD) is 64-bit
 #define GET_PREV_FREE(bp) (*((void**)(bp) + 1))
 
 #define SEGLIST_SIZE 10
+#define SEGLIST_AUTO ((size_t)-1)
 
 static char* heap_begin = NULL;
 static void** seglist;
@@ -142,10 +152,15 @@ static void** seglist;
 // Helper function declarations
 static void* extend_heap(size_t);
 static void* coalesce(void*);
+static void* find_fit(size_t);
+static void place(void*, size_t);
 
+void mm_checkheap_alter(const char*, int);
+
+static size_t seglist_get_index(size_t);
 static void seglist_insert(void*, size_t);
 static void seglist_remove(void*, size_t);
-static void* seglist_find(size_t);
+static void* seglist_find(size_t, size_t);
 
 /**
  * @brief Initialize dynamic allocator
@@ -162,10 +177,14 @@ int mm_init(void) {
   }
   heap_begin = (char*)(seglist + SEGLIST_SIZE);
   PUT_WORD(heap_begin, 0);  // Aligment padding
+  // prologue header
   PUT_PACK(heap_begin + 1 * WORD_SIZE, DWORD_SIZE, BTAG_FREE, 1);
+  // prologue footer
   PUT_PACK(heap_begin + 2 * WORD_SIZE, DWORD_SIZE, BTAG_FREE, 1);
+  // epilogue header
   PUT_PACK(heap_begin + 3 * WORD_SIZE, WORD_SIZE, BTAG_FREE, 1);
   if (extend_heap(INIT_SIZE) == NULL) return -1;
+  CHECK_HEAP();
   return 0;
 }
 
@@ -175,7 +194,25 @@ int mm_init(void) {
  * @param size The size of allocated memory.
  * @return Pointer to allocated memory with ALIGNMENT
  */
-void* malloc(size_t size) { return NULL; }
+void* malloc(size_t size) {
+  size_t allocated_size;
+  if (size == 0) return NULL;
+  if (size <= MIN_PAYLOAD_SIZE)
+    allocated_size = MIN_BLOCK_SIZE;
+  else
+    allocated_size = ALIGN(WORD_SIZE + size);
+  void* bp;
+  if ((bp = find_fit(allocated_size))) {
+    place(bp, allocated_size);
+    return bp;
+  }
+  size_t extended_size = MAX(allocated_size, CHUNK_SIZE);
+  if ((bp = extend_heap(extended_size / WORD_SIZE))) {
+    place(bp, allocated_size);
+    return bp;
+  }
+  return NULL;
+}
 
 /**
  * @brief Free a block allocated by @c malloc , @c realloc or @c calloc
@@ -184,6 +221,12 @@ void* malloc(size_t size) { return NULL; }
  */
 void free(void* ptr) {
   if (!ptr) return;
+  size_t size = GET_SIZE(GET_HEADER(ptr));
+  PUT_PACK(GET_HEADER(ptr), size, BTAG_KEEP, 0);
+  PUT_PACK(GET_FOOTER(ptr), size, BTAG_KEEP, 0);
+  void* next_block = GET_NEXT_BLOCK(ptr);
+  PUT_FREE_BTAG(GET_HEADER(next_block));
+  coalesce(ptr);
 }
 
 /**
@@ -194,7 +237,20 @@ void free(void* ptr) {
  * @param size New block size
  * @return Point to new allocated block
  */
-void* realloc(void* oldptr, size_t size) { return NULL; }
+void* realloc(void* oldptr, size_t size) {
+  if (size == 0) {
+    free(oldptr);
+    return NULL;
+  }
+  if (oldptr == NULL) {
+    return malloc(size);
+  }
+  void* newptr;
+  if ((newptr = malloc(size)) == NULL) return NULL;
+  memcpy(newptr, oldptr, MIN(size, GET_SIZE(GET_HEADER(oldptr))));
+  free(oldptr);
+  return newptr;
+}
 
 /**
  * @brief Allocation with initialization
@@ -203,7 +259,13 @@ void* realloc(void* oldptr, size_t size) { return NULL; }
  * @param size Size of each element
  * @return Pointer to first element allocated
  */
-void* calloc(size_t nmemb, size_t size) { return NULL; }
+void* calloc(size_t nmemb, size_t size) {
+  size_t total_size = size * nmemb;
+  void* bp = malloc(total_size);
+  if (!bp) return NULL;
+  memset(bp, 0, total_size);
+  return bp;
+}
 
 /*
  * Return whether the pointer is in the heap.
@@ -222,9 +284,59 @@ static int aligned(const void* p) { return (size_t)ALIGN(p) == (size_t)p; }
 /**
  * @brief Heap consistency checker
  * Scans the heap and checks it for correctness
+ * @param func Pass __func__ into it for debugging
  * @param lineno Pass __LINE__ macro into it for debugging
  */
-void mm_checkheap(int lineno) {}
+void mm_checkheap_alter(const char* func, int lineno) {
+#define ch_printf(FORMAT, ...) \
+  dbg_printf("%s(%d): " FORMAT, func, lineno, ##__VA_ARGS__)
+  void* bp;
+  void* header;
+  void* footer;
+  void* heap_end = (char*)mem_heap_hi() + 1;
+  if ((heap_begin - (char*)(mem_heap_lo())) != SEGLIST_SIZE * DWORD_SIZE) {
+    ch_printf("Seglist pointers don't have enough space.\n");
+  }
+  // Prologue checking
+  bp = heap_begin;
+  header = GET_HEADER(bp);
+  footer = GET_FOOTER(bp);
+  if (GET_SIZE(header) != DWORD_SIZE || GET_ALLOC(header) != 1) {
+    ch_printf("Prologue block smashed: wrong size\n");
+    ch_printf("Prologue SIZE: %d\n", GET_SIZE(header));
+    ch_printf("Prologue ALLOC: %d\n", GET_ALLOC(header));
+    exit(EXIT_FAILURE);
+  }
+  if (GET_SIZE(header) != GET_SIZE(footer) ||
+      GET_ALLOC(header) != GET_ALLOC(footer)) {
+    ch_printf("Prologue block smashed: mismatched h/f\n");
+    ch_printf("Prologue header: (%d, %d)", GET_SIZE(header), GET_ALLOC(header));
+    ch_printf("Prologue footer: (%d, %d)", GET_SIZE(footer), GET_ALLOC(footer));
+    exit(EXIT_FAILURE);
+  }
+  // Epilogue checking
+  bp = heap_end;
+  header = GET_HEADER(bp);
+  footer = GET_FOOTER(bp);
+  if (GET_SIZE(header) != WORD_SIZE || GET_ALLOC(header) != 1) {
+    ch_printf("Epilogue block smashed: wrong size\n");
+    ch_printf("Epilogue SIZE: %d\n", GET_SIZE(header));
+    ch_printf("Epilogue ALLOC: %d\n", GET_ALLOC(header));
+    exit(EXIT_FAILURE);
+  }
+  // Alignment checking
+  for (bp = heap_begin; bp != heap_end; bp = GET_NEXT_BLOCK(bp)) {
+    if (!in_heap(bp)) {
+      ch_printf("Block %p not in heap (%p:%p)", bp, mem_heap_hi(), mem_heap_lo());
+      exit(EXIT_FAILURE);
+    }
+  }
+  // H/f checking
+
+#undef ch_printf
+}
+
+void mm_checkheap(int lineno) {}  // Shut up linker complaint.
 
 // Helper function definitions
 
@@ -241,18 +353,20 @@ static void* extend_heap(size_t words) {
   if ((bp = mem_sbrk(ext_size)) == ERRPTR) return NULL;
   PUT_PACK(GET_HEADER(bp), ext_size, BTAG_FREE, 0);
   PUT_PACK(GET_FOOTER(bp), ext_size, BTAG_FREE, 0);
+  // dummy header
   PUT_PACK(GET_HEADER(GET_NEXT_BLOCK(bp)), WORD_SIZE, BTAG_FREE, 0);
   return coalesce(bp);
 }
 
 /**
- * @brief
+ * @brief Merge two adjacent free blocks
  *
- * @param bp
- * @return void*
+ * @param bp One of the block to be merged
+ * @return Pointer to new merged block
  */
 static void* coalesce(void* bp) {
-  WORD prev_alloc = GET_ALLOC(GET_FOOTER(GET_PREV_BLOCK(bp)));
+  // WORD prev_alloc = GET_ALLOC(GET_FOOTER(GET_PREV_BLOCK(bp)));
+  WORD prev_alloc = GET_BTAG(GET_HEADER(bp)) == BTAG_ALLOC;
   WORD next_alloc = GET_ALLOC(GET_HEADER(GET_NEXT_BLOCK(bp)));
   size_t size = GET_SIZE(GET_HEADER(bp));
   if (prev_alloc && next_alloc) {
@@ -283,6 +397,45 @@ static void* coalesce(void* bp) {
   seglist_insert(bp, size);
   return bp;
 }
+
+/**
+ * @brief Get an appropriate free block depends on @c size
+ *
+ * @param size The size of required block
+ * @return Pointer to a fit block
+ */
+static void* find_fit(size_t size) {
+  size_t index = seglist_get_index(size);
+  void* fp = NULL;
+  // find next seglist if previous not found
+  for (; index < SEGLIST_SIZE && (fp = seglist_find(index, size)) == NULL;
+       index++)
+    ;
+  return fp;
+}
+
+/**
+ * @brief Place allocated block inside a free block
+ * Split a free block if available
+ * @param ptr Where to place
+ * @param alloc_size Allocated size
+ */
+static void place(void* ptr, size_t alloc_size) {
+  size_t free_size = GET_SIZE(GET_HEADER(ptr));
+  seglist_remove(ptr, free_size);
+  ptrdiff_t diff = free_size - alloc_size;
+  if (diff >= MIN_BLOCK_SIZE) {
+    PUT_PACK(GET_HEADER(ptr), alloc_size, BTAG_KEEP, 1);
+    void* bp = GET_NEXT_BLOCK(ptr);
+    PUT_PACK(GET_HEADER(bp), diff, BTAG_ALLOC, 0);
+    PUT_PACK(GET_FOOTER(bp), diff, BTAG_ALLOC, 0);
+    seglist_insert(bp, diff);
+  } else {
+    PUT_PACK(GET_HEADER(ptr), alloc_size, BTAG_KEEP, 1);
+  }
+}
+
+// Begin seglist helper function
 
 /**
  * @brief Determine which seglist to use base on @c size
@@ -355,16 +508,19 @@ static void seglist_remove(void* fp, size_t size) {
 /**
  * @brief Find appropriate free block in seglist
  * The minimum block contains @c size bytes
+ * @param index Index of seglist or @c SEGLIST_AUTO
  * @param size Size of target
  * @return Pointer to the fit block, @c NULL if fit not fround
  */
-static void* seglist_find(size_t size) {
-  size_t index = seglist_get_index(size);
-  char* fp = seglist[index];
-  dbg_printf("List start address %p", fp);
+static void* seglist_find(size_t index, size_t size) {
+  if (index == SEGLIST_AUTO) index = seglist_get_index(size);
+  void* fp = seglist[index];
+  // dbg_printf("List start address %p", fp);
   while (fp) {
     if (size <= GET_SIZE(GET_HEADER(fp))) return fp;
     fp = GET_NEXT_FREE(fp);
   }
   return NULL;
 }
+
+// End seglist helper function
